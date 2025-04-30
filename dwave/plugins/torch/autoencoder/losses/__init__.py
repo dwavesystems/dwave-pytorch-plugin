ebfa79a21a360c4c20849b86c554f3e506eb1526
Vladimir Vargas Calderón <vvargasc@dwavesys.com>
2025-04-30 15:30:00 -0500

    remove mmd loss

--- dwave/plugins/torch/autoencoder/losses/__init__.py
+++ dwave/plugins/torch/autoencoder/losses/__init__.py
@@ -1,4 +1,3 @@
 from dwave.plugins.torch.autoencoder.losses.kl_divergence import (
     pseudo_kl_divergence_loss,
 )
-from dwave.plugins.torch.autoencoder.losses.mmd import RadialBasisFunction, mmd_loss
