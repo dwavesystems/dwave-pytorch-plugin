f9fb7da024b282297975e2d87674ee515f3dfe8d
Mohsen Rahmani <54963566+mhramani@users.noreply.github.com>
2025-07-22 07:06:57 -0700

    Update tests/test_boltzmann_machine.py
    Co-authored-by: Theodor Isacsson <theodor@isacsson.ca>

--- tests/test_boltzmann_machine.py
+++ tests/test_boltzmann_machine.py
@@ -135,7 +135,7 @@ class TestGraphRestrictedBoltzmannMachine(unittest.TestCase):
 
     def test_compute_effective_field(self):
         grbm = GRBM([0, 1, 2], [(0, 1), (0, 2), (1, 2)], [2])
-        # Note : In the digram bellow linear biases are shown using  <>
+        # Note : In the diagram below linear biases are shown using  <>
         #        quadratic biases using (), and spin value of visibles using []
         #               (0.13)
         # Model: 2 <.4> ------- 0 [-1]
