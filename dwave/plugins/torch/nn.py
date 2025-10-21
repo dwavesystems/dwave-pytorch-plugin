495851c072ec9dee63c039301c6ffce0cbf43177
kchern <kchern@dwavesys.com>
2025-10-21 21:42:16 +0000

    Address review comments

--- dwave/plugins/torch/nn.py
+++ dwave/plugins/torch/nn.py
@@ -1,112 +1,121 @@
+# Copyright 2025 D-Wave
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#   http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+from __future__ import annotations
+
 import inspect
+from typing import TYPE_CHECKING, Callable
+
+if TYPE_CHECKING:
+    from functools import partial
+
 from functools import wraps
 from types import MappingProxyType
 
 import torch
 from torch import nn
 
+__all__ = ["store_config", "SkipLinear", "LinearBlock"]
 
-def store_config(fn):
+
+def store_config(fn: Callable) -> partial:
+    """A decorator that tracks and stores arguments of methods (excluding ``self``).
+
+    Args:
+        fn (Callable[object, ...]): A method whose arguments will be stored in ``self.config``.
+
+    Returns:
+        partial: Wrapper function that stores argument of method.
+    """
     @wraps(fn)
     def wrapper(self, *args, **kwargs):
-        # Get signature of function and match the arguments with their names
+        """Store ``args``, ``kwargs``, and ``{"module_name": self.__class__.__name__}`` as a dictionary in ``self.config``.
+        """
         sig = inspect.signature(fn)
         bound = sig.bind(self, *args, **kwargs)
-        # Use default values if the args/kwargs were not supplied
         bound.apply_defaults()
-        config = {k: v for k, v in bound.arguments.items() if k != 'self'}
-        config['module_name'] = bound.args[0].__class__.__name__
+
+        config = {k: v for k, v in bound.arguments.items() if v != self}
+        config['module_name'] = self.__class__.__name__
         self.config = MappingProxyType(config)
+
         fn(self, *args, **kwargs)
     return wrapper
 
 
-class Identity(nn.Module):
-
-    @store_config
-    def __init__(self):
-        """An identity module.
-
-        This module is useful for handling cases where a neural network module is expected, but no
-        effect is desired."""
-        super().__init__()
-
-    def forward(self, x) -> torch.Tensor:
-        """Input
-
-        Args:
-            x (torch.Tensor): The input and the output.
-
-        Returns:
-            torch.Tensor: The input and the output.
-        """
-        return x
-
-
 class SkipLinear(nn.Module):
+    """Applies a linear transformation to the incoming data: :math:`y = xA^T`.
+
+    This module is identity when ``din == dout``, otherwise, it is a linear transformation, i.e.,
+    no bias term.
+
+    Args:
+        din (int): Size of each input sample.
+        dout (int): Size of each output sample.
+    """
 
     @store_config
-    def __init__(self, din, dout) -> None:
-        """Applies a linear transformation to the incoming data: :math:`y = xA^T`.
-
-        This module is identity when `din == dout`, otherwise, it is a linear transformation, i.e.,
-        no bias term.
-
-        Args:
-            din (int): Size of each input sample.
-            dout (int): Size of each output sample.
-        """
+    def __init__(self, din: int, dout: int) -> None:
         super().__init__()
         if din == dout:
-            self.linear = Identity()
+            self.linear = nn.Identity()
         else:
             self.linear = nn.Linear(din, dout, bias=False)
 
-    def forward(self, x) -> torch.Tensor:
-        """Apply a linear transformation to the input variable `x`.
+    def forward(self, x: torch.Tensor) -> torch.Tensor:
+        """Apply a linear transformation to the input variable ``x``.
 
         Args:
-            x (torch.Tensor): the input tensor.
+            x (torch.Tensor): The input tensor.
 
         Returns:
-            torch.Tensor: the linearly-transformed tensor of `x`.
+            torch.Tensor: The linearly-transformed tensor of ``x``.
         """
         return self.linear(x)
 
 
 class LinearBlock(nn.Module):
+    """A linear block consisting of normalizations, linear transformations, dropout, relu, and a skip connection.
+
+    The module is composed of (in order):
+    1. a first layer norm,
+    2. a first linear transformation,
+    3. a dropout,
+    4. a relu activation,
+    5. a second layer norm,
+    6. a second linear layer, and, finally,
+    7. a skip connection from initial input to output.
+
+    Args:
+        din (int): Size of each input sample.
+        dout (int): Size of each output sample.
+        p (float): Dropout probability.
+    """
     @store_config
-    def __init__(self, din, dout, p) -> None:
-        """A linear block consisting of normalizations, linear transformations, dropout, relu, and a skip connection.
-
-        The module is composed of (in order):
-        1. a first layer norm,
-        2. a first linear transformation,
-        3. a dropout,
-        4. a relu activation,
-        5. a second layer norm,
-        6. a second linear layer, and, finally,
-        7. a skip connection from initial input to output.
-
-        Args:
-            din (int): Size of each input sample
-            dout (int): Size of each output sample
-            p (float): Dropout probability.
-        """
+    def __init__(self, din: int, dout: int, p: float) -> None:
         super().__init__()
-        self.skip = SkipLinear(din, dout)
-        linear_1 = nn.Linear(din, dout)
-        linear_2 = nn.Linear(dout, dout)
+        self._skip = SkipLinear(din, dout)
         self.block = nn.Sequential(
             nn.LayerNorm(din),
-            linear_1,
+            nn.Linear(din, dout),
             nn.Dropout(p),
             nn.ReLU(),
             nn.LayerNorm(dout),
-            linear_2,
+            nn.Linear(dout, dout),
         )
 
-    def forward(self, x) -> torch.Tensor:
+    def forward(self, x: torch.Tensor) -> torch.Tensor:
         """Transforms the input `x` with the modules.
 
         Args:
@@ -115,4 +124,4 @@ class LinearBlock(nn.Module):
         Returns:
             torch.Tensor: Output tensor.
         """
-        return self.block(x) + self.skip(x)
+        return self.block(x) + self._skip(x)
