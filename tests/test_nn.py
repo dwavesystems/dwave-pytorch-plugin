9f31f15cae75641caae4298ea7bc2c9362d342ab
Vladimir Vargas Calderón <vvargasc@dwavesys.com>
2026-05-07 10:38:47 -0500

    Add Givens orthogonal layer
    Address orthogonal module PR feedback.

--- tests/test_nn.py
+++ tests/test_nn.py
@@ -14,10 +14,96 @@
 import unittest
 
 import torch
+import torch.nn as nn
+from einops import einsum
 from parameterized import parameterized
 
-from dwave.plugins.torch.nn import LinearBlock, SkipLinear, store_config
+from dwave.plugins.torch.nn import GivensRotation, LinearBlock, SkipLinear, store_config
 from tests.helper_functions import model_probably_good
+
+
+class NaiveGivensRotationLayer(nn.Module):
+    """Naive implementation of a Givens rotation layer.
+
+    Sequentially applies all Givens rotations to implement an orthogonal transformation in an order
+    provided by blocks, which are of shape (n_blocks, n/2, 2), and where usually each block contains
+    pairs of indices such that no index appears more than once in a block. However, this
+    implementation does not rely on that assumption, so that indeces can appear multiple times in a
+    block; however, all pairs of indices must appear exactly once in the entire blocks tensor.
+
+    Args:
+        in_features (int): Number of input features.
+        out_features (int): Number of output features.
+        bias (bool): If True, adds a learnable bias to the output. Default: True.
+
+    Note:
+        This layer defines an nxn SO(n) rotation matrix, so in_features must be equal to
+        out_features.
+    """
+
+    def __init__(self, in_features: int, out_features: int, bias: bool = True):
+        super().__init__()
+        assert in_features == out_features, (
+            "This layer defines an nxn SO(n) rotation matrix, so in_features must be equal to "
+            "out_features."
+        )
+        self.n = in_features
+        self.angles = nn.Parameter(torch.randn(in_features * (in_features - 1) // 2))
+        if bias:
+            self.bias = nn.Parameter(torch.zeros(out_features))
+        else:
+            self.register_parameter("bias", None)
+
+    def _create_rotation_matrix(self, angles, blocks: torch.Tensor | None = None):
+        """Creates the rotation matrix from the Givens angles by applying the Givens rotations in
+        order and sequentially, as specified by blocks.
+
+        Args:
+            angles (torch.Tensor): Givens rotation angles.
+            blocks (torch.Tensor | None, optional): Blocks specifying the order of rotations. If
+                None, all possible pairs of dimensions will be shaped into (n-1, n/2, 2) to create
+                the blocks. Defaults to None.
+
+        Returns:
+            torch.Tensor: Rotation matrix.
+        """
+        block_size = self.n // 2
+        if blocks is None:
+            # Create dummy blocks from triu indices:
+            triu_indices = torch.triu_indices(self.n, self.n, offset=1)
+            blocks = triu_indices.t().view(-1, block_size, 2)
+        U = torch.eye(self.n, dtype=angles.dtype)
+        for b, block in enumerate(blocks):
+            for k in range(block_size):
+                i = block[k, 0].item()
+                j = block[k, 1].item()
+                angle = angles[b * block_size + k]
+                c = torch.cos(angle)
+                s = torch.sin(angle)
+                Ge = torch.eye(self.n, dtype=angles.dtype)
+                Ge[i, i] = c
+                Ge[j, j] = c
+                Ge[i, j] = -s
+                Ge[j, i] = s
+                # Explicit Givens rotation
+                U = U @ Ge
+        return U
+
+    def forward(self, x: torch.Tensor, blocks: torch.Tensor) -> torch.Tensor:
+        """Applies the Givens rotation to the input tensor ``x``.
+
+        Args:
+            x (torch.Tensor): Input tensor of shape (..., n).
+            blocks (torch.Tensor): Blocks specifying the order of rotations.
+
+        Returns:
+            torch.Tensor: Rotated tensor of shape (..., n).
+        """
+        W = self._create_rotation_matrix(self.angles, blocks)
+        x = einsum(x, W, "... i, o i -> ... o")
+        if self.bias is not None:
+            x = x + self.bias
+        return x
 
 
 class TestUtils(unittest.TestCase):
@@ -77,6 +163,107 @@ class TestUtils(unittest.TestCase):
                              dict(a=123, b=1, x=5, y="hello", module_name="InnerModel"))
         self.assertDictEqual(dict(model.config["module_2"]),
                              dict(a="second", b=1, x=4, y="lol", module_name="InnerModel"))
+
+
+class TestOrthogonal(unittest.TestCase):
+
+    def test_bad_initialization_parameters(self):
+        with self.subTest("n less than 2"):
+            with self.assertRaisesRegex(ValueError, "n must be an integer greater than 1"):
+                GivensRotation(1)
+            with self.assertRaisesRegex(ValueError, "n must be an integer greater than 1"):
+                GivensRotation(0)
+            with self.assertRaisesRegex(ValueError, "n must be an integer greater than 1"):
+                GivensRotation(-5)
+        with self.subTest("n not integer"):
+            with self.assertRaisesRegex(ValueError, "n must be an integer greater than 1"):
+                GivensRotation(3.5)
+            with self.assertRaisesRegex(ValueError, "n must be an integer greater than 1"):
+                GivensRotation("a string")
+        with self.subTest("bias not boolean"):
+            with self.assertRaisesRegex(ValueError, "bias must be a boolean"):
+                GivensRotation(5, bias="another string")
+            with self.assertRaisesRegex(ValueError, "bias must be a boolean"):
+                GivensRotation(5, bias=1)
+
+    @parameterized.expand([9, 10])
+    def test_get_blocks_edges(self, n):
+        blocks = GivensRotation._get_blocks_edges(n).tolist()
+        # `blocks` is a list of lists of pairs, i.e., a list of blocks. Each block must contain
+        # pairs of dimensions such that each dimension appears only once per block.
+        # Also, across all blocks, each pair of dimensions must appear exactly once.
+        appeared_pairs = set()
+        for block in blocks:
+            appeared_dims = set()
+            for i, j in block:
+                self.assertNotIn(i, appeared_dims)
+                self.assertNotIn(j, appeared_dims)
+                appeared_dims.add(i)
+                appeared_dims.add(j)
+                pair = (min(i, j), max(i, j))
+                self.assertNotIn(pair, appeared_pairs)
+                appeared_pairs.add(pair)
+        # Check that all pairs appeared:
+        for i in range(n):
+            for j in range(i + 1, n):
+                pair = (i, j)
+                self.assertIn(pair, appeared_pairs)
+
+    @parameterized.expand([(n, bias) for n in [9, 10] for bias in [True, False]])
+    def test_GivensRotationLayer(self, n, bias):
+        din = n
+        dout = n
+        model = GivensRotation(n, bias=bias)
+        self.assertTrue(model_probably_good(model, (din,), (dout,)))
+
+    @parameterized.expand([(n, bias) for n in [9, 10] for bias in [True, False]])
+    def test_forward_agreement(self, n, bias):
+        layer = GivensRotation(n, bias=bias).double()
+        naive_layer = NaiveGivensRotationLayer(n, n, bias=bias).double()
+        blocks = layer.blocks
+        U_naive = naive_layer._create_rotation_matrix(layer.angles, blocks)
+        U_parallel = layer._create_rotation_matrix()
+
+        # Test that the matrices are close
+        self.assertTrue(torch.allclose(U_naive, U_parallel, atol=1e-6))
+
+        # Test orthogonality:
+        I = torch.eye(n, dtype=U_parallel.dtype)
+        UU_T = U_parallel @ U_parallel.T
+        self.assertTrue(torch.allclose(I, UU_T, atol=1e-6))
+
+        # Random input:
+        with torch.no_grad():
+            # forward pass will check consistency, so angles must be the same
+            naive_layer.angles.copy_(layer.angles)
+        x = torch.randn((7, n), dtype=U_parallel.dtype)  # batch size 7
+        y_naive = naive_layer(x, blocks)
+        y_parallel = layer(x)
+        self.assertTrue(torch.allclose(y_naive, y_parallel, atol=1e-6))
+
+    @parameterized.expand([(n, bias) for n in [9, 10] for bias in [True, False]])
+    def test_backward_agreement(self, n, bias):
+        layer = GivensRotation(n, bias=bias).double()
+        naive_layer = NaiveGivensRotationLayer(n, n, bias=bias).double()
+        blocks = layer.blocks
+
+        with torch.no_grad():
+            # forward and backward pass will check consistency, so angles must be the same
+            naive_layer.angles.copy_(layer.angles)
+
+        x = torch.randn((7, n), dtype=layer.angles.dtype)  # batch size 7
+
+        y_naive = naive_layer(x, blocks)
+        y_parallel = layer(x)
+
+        # Define some dummy loss, e.g. closeness to the identity:
+        loss_naive = torch.sum((y_naive - x) ** 2)
+        loss_parallel = torch.sum((y_parallel - x) ** 2)
+        loss_naive.backward()
+        loss_parallel.backward()
+        grad_parallel = layer.angles.grad
+        grad_naive = naive_layer.angles.grad
+        self.assertTrue(torch.allclose(grad_naive, grad_parallel, atol=1e-6))
 
 
 class TestLinear(unittest.TestCase):
